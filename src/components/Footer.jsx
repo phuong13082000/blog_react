ba13984ef35c1a326f5e702af79ca4c4a6976481
HoangPhuong <hoangphuong0813@gmail.com>
2023-05-27 02:09:45 +0700

    27052023

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,11 @@
 const Footer = () => {
 
     return (
-        <footer className="footer bg-white fixed bottom-0 left-0 right-0 text-center text-[#999] p-[2rem]"
-        style={{
-            borderTop: ".2rem solid var(--black)",
-            fontSize: "2rem",
-        }}>
-            <span className="text-blue-600">make by phuong 2023</span>
+        <footer
+            className="footer bg-white fixed bottom-0 left-0 right-0 text-center text-[#999] p-[2rem] dark:bg-black"
+            style={{borderTop: ".2rem solid black", fontSize: "2rem"}}
+        >
+            <span className="text-blue-600">Make by phuong 2023</span>
         </footer>
     );
 }
